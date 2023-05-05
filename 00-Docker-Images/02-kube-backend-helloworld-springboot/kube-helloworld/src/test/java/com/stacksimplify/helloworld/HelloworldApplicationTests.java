cd770d98024b8316c005222ed658e21a41dcdc73
pramodbn <begurnagaraj.p@northeastern.edu>
2023-05-04 19:39:27 -0500

    docker images

---
+++ 00-Docker-Images/02-kube-backend-helloworld-springboot/kube-helloworld/src/test/java/com/stacksimplify/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedevopsstore.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
